bb0ef14e07c7ca8ab02b59075fb1fe381422916f
Apeksha Thakre <thakreapeksha@gmail.com>
2026-01-09 17:21:35 +0530

    Day 4: Loopd for repetition and retries

---
+++ day-04-loops/LoopsDemo.java
@@ -0,0 +1,14 @@
+public class LoopsDemo {
+       public static void main(String[] args) {
+	System.out.println("For loop:");
+	for (int i = 0; i<=3; i++) {
+		System.out.println("Test Iteration " + i);
+	}
+		System.out.println("While loop:");
+		int count =1;
+		while (count <=3) {
+		System.out.println("Retry attempt " + count);	
+		count++;
+		}
+	}
+}
